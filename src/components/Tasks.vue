9528905340fabd2c929fe33daba50f6cd26332fa
César Álvarez <cesaralv97@gmail.com>
2021-04-14 13:36:13 +0200

    First commit

---
+++ src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- A key is required so we use the id of task -->
+    <div :key="task.id" v-for="task in tasks">
+        <Task :task="task" />
+    </div>
+</template>
+
+<script>
+    import Task from './Task'
+    export default {
+        name: 'Tasks',
+        props: {
+            tasks: Array
+        },
+        components: {
+            Task
+        }
+    }
+    
+
+</script>
+
